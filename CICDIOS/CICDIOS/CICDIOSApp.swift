99478ea57193bb55429c3f08fbf4e8380dca7f6c
Sudhakar Pachava <117287678+spachavainnominds@users.noreply.github.com>
2025-05-13 10:30:36 +0530

    Initial COde Push
    Initial Commit

---
+++ CICDIOS/CICDIOS/CICDIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDIOSApp.swift
+//  CICDIOS
+//
+//  Created by Sudhakar Pachava on 13/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CICDIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
