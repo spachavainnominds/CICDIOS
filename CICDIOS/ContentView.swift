e466f4a5435cf52952031ebc99bea77f8a22076f
Sudhakar Pachava <117287678+spachavainnominds@users.noreply.github.com>
2025-05-13 12:19:25 +0530

    Initial project creation

---
+++ CICDIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CICDIOS
+//
+//  Created by Sudhakar Pachava on 13/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Welcome to CICD ")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CICDIOSTests/CICDIOSTests.swift
@@ -0,0 +1,16 @@
+//
+//  CICDIOSTests.swift
+//  CICDIOSTests
+//
+//  Created by Sudhakar Pachava on 13/05/25.
+//
+
+import Testing
+
+struct CICDIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
